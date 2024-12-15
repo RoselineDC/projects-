66659dfd24c7c01252f6a87c1a9e677976dbfc7e
RoselineDC <roseline.danga@gmail.com>
2024-12-15 17:52:40 +0200

    improve

--- ontimesolutions/js/script.js
+++ ontimesolutions/js/script.js
@@ -1,5 +1,3 @@
-
-
 // slider function for welcome section
 let slideIndex = 0;
 showSlides();
@@ -8,17 +6,15 @@ showSlides();
     let slides = document.getElementsByClassName("slide");
     let dots = document.getElementsByClassName("dot");
     for (i  = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+        slides[i].style.display = "";
     }
     slideIndex++;
 
-    // for dot
     if (slideIndex > slides.length) {slideIndex = 1}
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
     slides[slideIndex-1].style.display = "flex";
     dots[slideIndex-1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 5 seconds
-
+    setTimeout(showSlides, 2000);
  }
